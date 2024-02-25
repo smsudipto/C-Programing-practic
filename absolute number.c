9ce89574dc36cf857d31d7304c24b72bd483c1c3
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2024-02-25 12:42:07 +0600

    Add files via upload

---
+++ absolute number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("please,enter your number:");
+    scanf("%d",&a);
+    int result= abs(a);
+    printf("The answer is %d: ",result);
+    return 0;
+}
