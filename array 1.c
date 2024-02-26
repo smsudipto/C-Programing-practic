434235fbbcd70bc18b1199f58f3fe84148998974
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2024-02-26 15:12:30 +0600

    Add files via upload

---
+++ array 1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int mark[5]={0,1,2,3,4};
+    printf("%d",mark[0]);
+    printf("%d",mark[1]);
+    printf("%d",mark[2]);
+    printf("%d",mark[3]);
+    printf("%d",mark[4]);
+}
