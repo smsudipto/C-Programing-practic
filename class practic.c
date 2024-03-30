fdeba3d211bffdb7e75daec3e214bbf15f1d2ee0
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2024-03-30 16:50:10 +0600

    Add files via upload

---
+++ class practic.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter your number a\n");
+    scanf("%d",&a);
+    printf("enter your number b\n");
+    scanf("%d"&b);
+    sum=a+b;
+    printf("sum is %d",sum);
+    return 0;
+
+}
