2e2b216e6e6029626f818aa6486227c4083c8a8c
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2024-04-22 22:42:58 +0600

    Add files via upload

---
+++ class6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main ()
+{
+    int num;
+scanf("%d",&num);
+switch(num%2==0);
+{
+case one:
+    printf(" even");
+    break;
+
+
+case 0:
+    {
+        printf("Odd")
+    }
+}
+    return 0;
+}
