c2f91f2b5f557cef4f340260f8694bc41d9965ee
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2024-07-08 17:04:43 +0600

    Add files via upload

---
+++ finding vowel using logical operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout<<"Enter your character:";
+    cin>>ch;
+    if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    {
+        cout<<"vowel";
+    }
+    else
+    {
+cout<<"consonant";
+    }
+}
