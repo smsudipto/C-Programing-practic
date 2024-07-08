831d0c7682ee99427b3fdd40247ea060f84b401c
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2024-07-08 17:04:29 +0600

    Add files via upload

---
+++ find vowel using relational operator.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout<<"Enter your character:";
+    cin>>ch;
+    if(ch=='A')
+    {
+        cout<<"vowel";
+    }
+    else if(ch=='E')
+    {
+        cout<<"vowel";
+    }
+    else if(ch=='I')
+    {
+        cout<<"vowel";
+    }
+    else if(ch=='O')
+    {
+        cout<<"vowel";
+    }
+    else if(ch=='U')
+    {
+        cout<<"vowel";
+    }
+    else
+    {
+cout<<"consonant";
+    }
+}
